002657dc7de1ad2b6023fbcbd17748e89591ae98
Vincent_PC <vincent.brochard01@etu.umontpellier.fr>
2025-11-04 00:48:58 +0100

    Ajustement parser

--- Ewrapped.py
+++ Ewrapped.py
@@ -10,7 +10,13 @@ import getpass
 parser =  argparse.ArgumentParser()
 
 parser.add_argument('name',type=str)
+parser.add_argument('output',type=str)
 
 filename = parser.parse_args().name
+output_name = parser.parse_args().output
 
 print(filename)
+print(output_name)
+
+
+
